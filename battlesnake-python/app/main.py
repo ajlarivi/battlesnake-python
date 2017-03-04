8921ca07105cc0f32dedb7e6c67cb16343ec222a
Chris Dodd <chrisdodd@uvic.ca>
2017-03-04 11:40:14 -0800

    will it move?

--- battlesnake-python/app/main.py
+++ battlesnake-python/app/main.py
@@ -8,13 +8,87 @@ def static(path):
     return bottle.static_file(path, root='static/')
 
 
-@bottle.post('/start')
+directions = ['up', 'down', 'left', 'right']
+
+def checkUp(kurt, grid):
+	head = kurt['coords'][0]
+	body = kurt['coords']
+	futureMove = [head[0], head[1]-1]
+	for coord in body:
+		if futureMove == coord:
+			return False
+		if futureMove[1] < 0:
+			return False
+	return True
+
+def checkLeft():
+	head = kurt['coords'][0]
+	body = kurt['coords']
+	futureMove = [head[0]-1, head[1]]
+	for coord in body:
+		if futureMove == coord:
+			return False
+		if futureMove[0] < 0:
+			return False
+	return True
+
+def checkRight():
+	head = kurt['coords'][0]
+	body = kurt['coords']
+	futureMove = [head[0]+1, head[1]
+	for coord in body:
+		if futureMove == coord:
+			return False
+		if futureMove[0] > 19:
+			return False
+	return True
+
+def checkDown():
+	head = kurt['coords'][0]
+	body = kurt['coords']
+	futureMove = [head[0], head[1]+1]
+	for coord in body:
+		if futureMove == coord:
+			return False
+		if futureMove[1] > 19:
+			return False
+	return True
+
+def noKill(kurt, grid);
+	if checkUp(kurt, grid):
+		legal.append('up')
+	if checkLeft(kurt, grid):
+		legal.append('left')
+	if checkRight(kurt, grid):
+		legal.append('right')
+	if checkDown(kurt, grid):
+		legal.append('down')
+	if legal:
+		return legal
+	else:
+		return "FUCK"
+
+init(data):
+    grid = [[0 for col in xrange(data['height'])] for row in xrange(data['width'])]
+    for snek in data['snakes']:
+        if snek['id']== ID:
+            kurt = snek
+        for coord in snek['coords']:
+            grid[coord[0]][coord[1]] = SNAKE
+
+    for wall in data['walls']:
+            grid[wall[0]][wall[1]] = WALL
+
+    for f in data['food']:
+        grid[f[0]][f[1]] = FOOD
+
+    return kurt, grid
+
 def start():
     data = bottle.request.json
     game_id = data['game_id']
     board_width = data['width']
     board_height = data['height']
-
     head_url = '%s://%s/static/head.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
@@ -33,10 +107,11 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+	kurt, grid = init(data)
+	legalMoves = noKill(kurt, grid)
     # TODO: Do things with data
-    directions = ['up', 'down', 'left', 'right']
     return {
-        'move': 'up',
+        'move': random.choice(legalMoves),
         'taunt': 'battlesnake-python!'
     }
 
